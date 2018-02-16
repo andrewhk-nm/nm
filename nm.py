645a948e2fd8dd7c8031078598477934ab257fda
Andrew Henning-Kolberg (nm) <andrew.hk@nm.com>
2018-02-16 11:11:50 -0500

    added some interest calculators

--- nm.py
+++ nm.py
@@ -6,6 +6,7 @@ Andrew Henning-Kolberg
 """
 
 import datetime
+import math
 from gross_net_calculator import gross_net_calculator as gnc
 
 def per_week(goal, have):
@@ -15,3 +16,34 @@ def per_week(goal, have):
     """
     return (goal - have) / (52 - datetime.datetime.isocalendar(
         datetime.datetime.now())[1])
+
+def ci(P, r, n, t):
+    """ "A = P(1 + r/n)**(nt)"
+    P is principal
+    r is interest rate
+    n is number of times interest is compounded per year
+    t is time in years
+    return A, the amount
+    """
+    return P * (1 + (r / n)) ** (n * t)
+
+def ci_list(P, r, n, t):
+    """ "A = P(1 + r/n)**(nt)"
+    P is principal
+    r is interest rate
+    n is number of times interest is compounded per year, as a list
+    t is time in years
+    return A, the amount
+    """
+    return_list = [P * (1 + (r / x)) ** (x * t) for x in n]
+    
+    return return_list
+
+def ci_pert(P, r, t):
+    """ "A = P*e**rt"
+    P is principal
+    r is interest rate
+    t is time in years
+    return A, the amount
+    """
+    return P * math.e ** (r * t)
