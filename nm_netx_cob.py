8b5a3fb27c7bbcca59a7250eb8cefd789f2024cc
Andrew Henning-Kolberg (nm) <andrew.hk@nm.com>
2018-04-20 13:01:52 -0400

    Parsing and Replacement works. Next, save new replacements

--- nm_netx_cob.py
+++ nm_netx_cob.py
@@ -10,6 +10,15 @@ translate it. Probably in a dictionary structure.
 #       the xls file that gets spit out of NetX actually looks like an html/xml file.
 #           I'm going to try the xml package instead of xlrd
 #       etree complains that the file isn't well formed. It indicates line 1column 198, which ends with 'x:str'
+# TODO: 2018-04-20 1p
+#       Parsing works.
+#       Replacement works.
+#       Requesting new verbiage works.
+#       TODO: Save the new verbiage.
+#       TODO: Make sure the format makes sense. I have two classes right now, is that the right amount?
+#               Or should there be just one? Or more?
+
+
 
 import json
 import xlrd
@@ -17,21 +26,43 @@ import xml.etree.ElementTree as etree
 from html.parser import HTMLParser
 
 class MyHTMLParser(HTMLParser):
+    fields = dict()
+    
     def handle_starttag(self, tag, attrs):
-        print("Encountered a start tag:", tag)
+        #print("Encountered a start tag:", tag)
+        pass
 
     def handle_endtag(self, tag):
-        print("Encountered an end tag :", tag)
+        #print("Encountered an end tag :", tag)
+        pass
 
     def handle_data(self, data):
-        print("Encountered some data  :", data) 
+        print("Encountered some data  :", data)
+        try:
+            replacement_data = self.fields[data]
+        except(KeyError):
+            replacement_data = input('"{}" does not have a replacement set.\n\nHow would you like this field to read instead?' .format(data))
 
-def read_cob_hmtl(filename=None):
-    """ Open filename (or prompt for one) and convert the NetX text into emailable text.
+        print("With Replacement       :", replacement_data)
+
+class Cob2Human():
+    """ Builds a dictionary to replace the gross COB text with human readable requests.
     """
-    if not filename:
-        filename = input('Please enter the full path and filename for the Client Onboarding Field Review xls/xml/html sheet.\n')
+
+    fields = dict()
+
+    def decodejson(self, filename='nm_netx_cob_data/nm_netx_cob.txt'):
+        #print(filename)
+        with open(filename, encoding='utf-8', mode='r') as a_file:
+            a_json = json.loads(a_file.readline())
+        return a_json
     
+    def __init__(self):
+        # Import the current json list of replacements, if it exists
+        self.fields = self.decodejson()
+        #print(self.fields)
+        
+  
 def read_cob_file(filename=None):
     """ Open filename (or prompt for one) and return a string of the file contents.
     """
@@ -48,10 +79,7 @@ def read_cob_file(filename=None):
 
         
 
-def decodejson(filename='nm_netx_cob_data/nm_netx_cob.txt'):
-    with open(filename, encoding='utf-8', mode='r') as a_file:
-        a_json = json.loads(a_file.readline())
-    return a_json
+
 
 
 def encodejson():
@@ -90,10 +118,9 @@ def writejson(fields, filename='nm_netx_cob_data/nm_netx_cob.txt'):
         a_file.write(json.dumps(fields))
 
 if __name__ == '__main__':
-    #encodejson()
-    #decoded_json = decodejson()
-    #print(decoded_json)
-    #read_cob_xl(r'C:\Users\perm7158\Documents\GitHub\nm\COBFieldReview-NS1394810 (not a real account, Roth IRA).xls')
-    #read_cob_xml(r'C:\Users\perm7158\Documents\GitHub\nm\COB.xls.html')
+    cob2human = Cob2Human()
+    print(cob2human.fields)
     parser = MyHTMLParser()
+    parser.fields = cob2human.fields
     parser.feed(read_cob_file(r'C:\Users\perm7158\Documents\GitHub\nm\COB.xls.html'))
+    
